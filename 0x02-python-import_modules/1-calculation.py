cafda5997dce19a81df8bb5c1827a072c38d93f3
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-01-06 21:01:17 +0200

    import and modules task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """ Prints the sum, subtration, multiplication and division os 10 and 5db"""
+    """ Print basic math operations with 10 and 5"""
     from calculator_1 import add, sub, div, mul
 
     a = 10
     b = 5
-    
     print(f'{a} + {b} = {add(a, b)}')
     print(f'{a} - {b} = {div(a, b)}')
     print(f'{a} * {b} = {mul(a, b)}')
