d8874399cd7ff2acd7e737f58a7e3b5a198cf873
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-01-06 21:07:52 +0200

    import and modules task 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Print basic math operations with 10 and 5"""
+    """Print basic math operations with 10 and 5."""
     from calculator_1 import add, sub, div, mul
 
     a = 10
