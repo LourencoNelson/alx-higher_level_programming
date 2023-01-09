4179a4778a93c0d94a0b0c56911a8a24b924297b
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-01-09 21:42:39 +0200

    inheritance task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
-def lookup(obj):
-    """Returns a list of all avaliable attributes and methods of an obj"""
-    return list(dir(obj))
+if __name__ == "__main__":
+    def lookup(obj):
+        """Returns a list of all avaliable attributes and methods of an obj"""
+        return list(dir(obj))
