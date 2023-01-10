1b4b6654c8fb8b2e14eacf6110cc045e514268f6
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-01-10 11:45:48 +0200

    if-else, loops, functions task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,6 +2,6 @@
 
 def islower(c):
     if ord(c) in range(97, 123):
-        print("{} is lower".format(c))
+        return True
     else:
-        print("{} is upper".format(c))
+        return False
