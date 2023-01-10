982734371f37a1d802fb7521c557696c95890c76
Lourenco Nelson <lourenco.n.manhica@gmail.com>
2023-01-10 12:00:15 +0200

    if-else, loops, functions task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,6 @@
 def uppercase(str):
     for s in str:
         if s.islower():
-            print("{}".format(char(ord(s)-32)), end="")
+            print("{}".format(chr(ord(s)-32)), end="")
         else:
             print("{}".format(s), end="")
